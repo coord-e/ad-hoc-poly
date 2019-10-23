5b7115389685ed8395682976b7000ad1e7e522ef
coord.e <me@coord-e.com>
2019-10-23 07:54:11 +0000

    Add: Code source AST

--- src/AST/Source.hs
+++ src/AST/Source.hs
@@ -1,1 +1,48 @@
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
 module AST.Source where
+
+import           Control.Lens.TH
+import           Data.Functor.Foldable.TH
+
+type TVarName = String
+type TypeName = String
+type Name = String
+
+data Type
+  = TInt
+  | TChar
+  | TStr
+  | TVar TVarName
+  | TName TypeName
+  | TFun Type Type
+  | TApp Type Type
+  | TLam TypeName Type
+  | TTuple [Type]
+  | TConstraint Name Type
+
+makeBaseFunctor ''Type
+
+data TypeScheme
+  = Forall { _vars        :: [TVarName],
+             _constraints :: [Type],
+             _type_       :: Type }
+
+makeLenses ''TypeScheme
+
+data Expr
+  = Int Int
+  | Char Char
+  | Str String
+  | Var Name
+  | App Expr Expr
+  | Lam Name Expr
+  | Tuple [Expr]
+  | Let Name Expr Expr
+  | Type TypeName Type Expr
+  | Satisfy TypeScheme Expr Expr
+
+makeBaseFunctor ''Expr
