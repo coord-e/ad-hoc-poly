9e5f38850dacb4481dc8f097045f4bb385740914
coord.e <me@coord-e.com>
2019-10-25 04:18:15 +0000

    Add: Add a sematic definition of types

--- src/Compile/Type.hs
+++ src/Compile/Type.hs
@@ -1,6 +1,38 @@
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
 module Compile.Type where
 
-import           AST.Source
+import qualified AST.Source               as S
 
-eval :: Type -> Type
-eval = id
+import           Control.Lens.TH
+import           Data.Functor.Foldable.TH
+
+
+data Type
+  = TInt
+  | TChar
+  | TStr
+  | TVar S.TVarName
+  | TFun Type Type
+  | TLam S.TypeName Type
+  | TTuple [Type]
+  | TConstraint S.Name Type
+  deriving Show
+
+makeBaseFunctor ''Type
+
+
+data TypeScheme
+  = Forall { _vars        :: [S.TVarName],
+             _constraints :: [Type],
+             _type_       :: Type }
+  deriving Show
+
+makeLenses ''TypeScheme
+
+
+eval :: S.Type -> Type
+eval _ = TInt
