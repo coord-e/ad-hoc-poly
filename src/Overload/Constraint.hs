e0e95facd5959edfba3ffff18773f58fcc597a65
coord.e <me@coord-e.com>
2019-10-25 15:07:11 +0000

    Add: Implement an instance relation

---
+++ src/Overload/Constraint.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE FlexibleContexts #-}
+module Overload.Constraint where
+
+import           Overload.Env
+import           Overload.Subst
+import           Overload.Type
+import           Overload.Var
+
+import           Control.Eff
+import           Control.Eff.Reader.Strict
+import           Control.Eff.State.Strict
+import           Control.Lens
+import           Control.Monad.Extra       (allM, maybeM)
+import qualified Data.Map                  as Map
+import qualified Data.Set                  as Set
+
+
+-- isInstance a b returns True if b is more general than a.
+isInstance :: (Member (State Infer) r, Member (Reader Env) r) => TypeScheme -> TypeScheme -> Eff r Bool
+isInstance (Forall as1 p1) t2@(Forall as2 p2) = do
+  ts <- mapM (fmap TVar . const fresh) as2
+  let s1 = Subst $ Map.fromList $ zip as1 ts
+  let s2 = Subst $ Map.fromList $ zip as2 ts
+  (&& Set.disjoint (ftv t2) (Set.fromList as1)) <$> isInstancePred (apply s1 p1) (apply s2 p2)
+
+isInstancePred :: (Member (State Infer) r, Member (Reader Env) r) => PredType -> PredType -> Eff r Bool
+isInstancePred (PredType cs1 t1) (PredType cs2 t2) = (t1 == t2 &&) <$> allM go cs2
+  where
+    go c@(Constraint x t) = do
+      b <- bound x t
+      i <- inst x t
+      return (c `elem` cs1 || b || i)
+    bound x t = maybe False (views _1 (== poly t)) <$> reader (views (context . bindings) (Map.lookup x))
+    inst x t = maybeM (return False) (views _1 (isInstance (poly t))) $ reader (views (context . instantiations) (Map.lookup x))
+    poly t = Forall [] (PredType [] t)
