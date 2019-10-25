9d6a21bdf9e3aa4e2f5ad55cc4de3fbfc13bf004
coord.e <me@coord-e.com>
2019-10-25 08:45:03 +0000

    Add: Add types expressing normalized types

--- src/Overload/Type.hs
+++ src/Overload/Type.hs
@@ -1,3 +1,49 @@
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
 module Overload.Type (Type(..), TypeF(..), TypeScheme(..)) where
 
-import           AST.Source
+import qualified AST.Source               as S
+
+import           Control.Lens.TH
+import           Data.Functor.Foldable.TH
+
+
+type TyVarId = Int
+
+data TyVar
+  = Unbound TyVarId Int
+  | Bound TyVarId
+  deriving Show
+
+-- normalized type (Star kind)
+data Type
+  = TInt
+  | TChar
+  | TStr
+  | TVar TyVar
+  | TFun Type Type
+  | TTuple [Type]
+  deriving Show
+
+makeBaseFunctor ''Type
+
+
+-- normalized type (Constraint kind)
+data Constraint
+  = Constraint { _name        :: S.TypeName
+               , _requirement :: Type }
+  deriving Show
+
+makeLenses ''Constraint
+
+
+data TypeScheme
+  = Forall { _vars        :: [TyVarId],
+             _constraints :: [Constraint],
+             _type_       :: Type }
+  deriving Show
+
+makeLenses ''TypeScheme
