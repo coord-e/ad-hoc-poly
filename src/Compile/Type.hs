64dfc8d45bf6963cdb7545597985bf5dbae3532c
coord.e <me@coord-e.com>
2019-10-25 07:05:29 +0000

    Add: Add a type evaluator, resulting the same type from AST

--- src/Compile/Type.hs
+++ src/Compile/Type.hs
@@ -1,34 +1,3 @@
-{-# LANGUAGE DeriveFoldable    #-}
-{-# LANGUAGE DeriveFunctor     #-}
-{-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeFamilies      #-}
-module Compile.Type where
+module Compile.Type (Type(..), TypeF(..), TypeScheme(..)) where
 
-import qualified AST.Source               as S
-
-import           Control.Lens.TH
-import           Data.Functor.Foldable.TH
-
-
-data Type
-  = TInt
-  | TChar
-  | TStr
-  | TVar S.TVarName
-  | TFun Type Type
-  | TLam S.TypeName Type
-  | TTuple [Type]
-  | TConstraint S.Name Type
-  deriving Show
-
-makeBaseFunctor ''Type
-
-
-data TypeScheme
-  = Forall { _vars        :: [S.TVarName],
-             _constraints :: [Type],
-             _type_       :: Type }
-  deriving Show
-
-makeLenses ''TypeScheme
+import           AST.Source
--- src/Compile/TypeEval.hs
+++ src/Compile/TypeEval.hs
@@ -4,12 +4,26 @@ module Compile.TypeEval where
 import qualified AST.Source                as S
 import           Compile.Env
 import           Compile.Type
-import           Reporting.Error
 
 import           Control.Eff
-import           Control.Eff.Exception
 import           Control.Eff.Reader.Strict
+import           Control.Lens
+import           Data.Functor.Foldable
+import qualified Data.Map                  as Map
 
 
-eval :: (Member (Reader Env) r, Member (Exc Error) r) => S.Type -> Eff r Type
-eval _ = return TInt
+eval :: Member (Reader Env) r => Type -> Eff r Type
+eval (TName x)               = reader $ flip (Map.!) x . view typeEnv
+eval (TFun t1 t2)            = TFun <$> eval t1 <*> eval t2
+eval (TTuple ts)             = TTuple <$> mapM eval ts
+eval (TConstraint x t)       = TConstraint x <$> eval t
+eval (TApp t1 t2) = do
+  t1' <- eval t1
+  t2' <- eval t2
+  let (TLam x body) = t1'
+  eval $ cata (subst x t2') body
+eval t = return t
+
+subst :: S.TypeName -> Type -> TypeF Type -> Type
+subst n r (TNameF x) | x == n = r
+subst _ _ t          = embed t
