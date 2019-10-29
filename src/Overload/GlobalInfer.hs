f5c5cf45f05cebce763deb8fa6fff30884399baa
coord.e <me@coord-e.com>
2019-10-29 07:08:02 +0000

    Add: Add missing untracked source

---
+++ src/Overload/GlobalInfer.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE DataKinds #-}
+module Overload.GlobalInfer where
+
+import qualified AST.Source                as S
+import qualified AST.Target                as T
+import           Overload.Env
+import           Overload.Instance
+import           Overload.LocalInfer
+import           Overload.Subst
+import           Overload.Type
+import           Overload.Unify
+import           Reporting.Error
+
+import           Control.Eff
+import           Control.Eff.Exception
+import           Control.Eff.Extend
+import           Control.Eff.Fresh
+import           Control.Eff.Reader.Strict
+import           Control.Eff.State.Strict
+import           Control.Eff.Writer.Strict
+import           Data.Functor.Foldable
+import qualified Data.IntMap               as IntMap
+
+
+type PSubst = IntMap.IntMap T.Expr
+
+
+-- TODO: implement without primitive recursion
+scanWaitList :: Subst -> WaitList -> (PredType, T.Expr, PSubst) -> Eff '[Writer S.Name, Exc Error, Fresh, Reader Env, State Constraints] (PredType, T.Expr, PSubst)
+scanWaitList _ [] acc = return acc
+scanWaitList s (Candidate i x p:wl) (ap, ae, m) = do
+  inst <- findInstantiation x . scheme $ apply s p
+  case inst of
+    Just (_, e) -> do
+      -- TODO: can unification made here be ignored in the scan?
+      (_, e', wl') <- raise $ runLocalInfer e
+      scanWaitList s (wl ++ wl') (ap, ae, IntMap.insert i e' m)
+    Nothing -> do
+      n <- freshn x
+      tell n
+      let (PredType cs t) = ap
+      let (PredType cs' t') = p
+      let c = Constraint x $ scheme p
+      scanWaitList s wl (PredType (c:cs++cs') (TFun t' t), T.Lam n ae, IntMap.insert i (T.Var n) m)
+
+runScanWaitList :: Subst -> PredType -> T.Expr -> WaitList -> Eff '[Exc Error, Fresh, Reader Env, State Constraints] (PredType, T.Expr, PSubst, [S.Name])
+runScanWaitList s p e wl = do
+  ((p', e', m), left) <- runListWriter $ scanWaitList s wl (p, e, IntMap.empty)
+  return (p', e', m, left)
+
+globalInfer :: S.Expr -> Eff '[Exc Error, Fresh, Reader Env, State Constraints] (PredType, T.Expr, [S.Name])
+globalInfer e = do
+  (p, e', waitlist) <- runLocalInfer e
+  subst <- solve =<< get  -- TODO: save this solve and use later?
+  (p', e'', m, left) <- runScanWaitList subst p e' waitlist
+  return (p', cata (resolvePlaceholders m) e'', left)
+
+resolvePlaceholders :: PSubst -> T.ExprF T.Expr -> T.Expr
+resolvePlaceholders s (T.PlaceholderF i) = s IntMap.! i
+resolvePlaceholders _ e                  = embed e
