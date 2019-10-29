c4abe8434e2e10bcedd0944adc32faadc7862bc1
coord.e <me@coord-e.com>
2019-10-29 14:47:19 +0000

    Add: Implement configuration loader of yaml file

--- src/Config.hs
+++ src/Config.hs
@@ -1,26 +1,62 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Config where
 
-import           Control.Lens.TH
-import qualified Data.Map        as Map
+import           AST.Source
+import qualified Parse.Internal         as P (Parser)
+import           Parse.Kind             (kind)
+import           Parse.Type             (typeScheme)
+import           Reporting.Error
+import           Reporting.Error.Config
+
+import           Data.Aeson.Types       (typeMismatch)
+import qualified Data.Map               as Map
+import           Data.Text
+import           Data.Yaml
+import           Text.Megaparsec        (errorBundlePretty, parse)
 
 
 data LiteralTypes
-  = LiteralTypes { integer :: String
-                 , real    :: String
-                 , char    :: String
-                 , bool    :: String
-                 , string  :: String }
+  = LiteralTypes { integer :: TypeScheme
+                 , real    :: TypeScheme
+                 , char    :: TypeScheme
+                 , boolean :: TypeScheme
+                 , string  :: TypeScheme }
 
 data Config
-  = Config { _types        :: Map.Map String String
-           , _literalTypes :: LiteralTypes
-           , _bindings     :: Map.Map String String }
-
-makeLenses ''Config
+  = Config { baseTypes    :: Map.Map String Kind
+           , literalTypes :: LiteralTypes
+           , bindings     :: Map.Map String TypeScheme }
 
 
-loadDefaultConfigFile :: IO Config
-loadDefaultConfigFile = return $ Config (Map.fromList [("Int", "*"), ("Char", "*"), ("Float", "*"), ("String", "*"), ("Bool", "*")])
-                                 (LiteralTypes "Int" "Float" "Char" "Bool" "String")
-                                 (Map.fromList [("eqInt", "Int -> Int -> Bool"), ("eqChar", "Char -> Char -> Bool")])
+instance FromJSON TypeScheme where
+  parseJSON (String text) = parseAndBundle typeScheme text
+  parseJSON o             = typeMismatch "String" o
+
+instance FromJSON Kind where
+  parseJSON (String text) = parseAndBundle kind text
+  parseJSON o             = typeMismatch "String" o
+
+instance FromJSON LiteralTypes where
+  parseJSON (Object v) = do
+    integer <- v .: "integer"
+    real <- v .: "real"
+    char <- v .: "char"
+    boolean <- v .: "boolean"
+    string <- v .: "string"
+    return $ LiteralTypes { integer, real, char, boolean, string }
+  parseJSON o = typeMismatch "Object" o
+
+instance FromJSON Config where
+  parseJSON (Object v) = Config
+    <$> v .: "base_types"
+    <*> v .: "literal_types"
+    <*> v .: "bindings"
+  parseJSON o = typeMismatch "Object" o
+
+
+parseAndBundle :: P.Parser a -> Text -> Parser a
+parseAndBundle p input =
+  case parse p "" input of
+    Left err -> fail $ errorBundlePretty err
+    Right s  -> return s
