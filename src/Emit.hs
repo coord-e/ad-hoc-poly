583c9406e12ef21d4e6f9dc3409f813ffffcb5cd
coord.e <me@coord-e.com>
2019-10-29 16:58:57 +0000

    Fix: Wrap arguments with parentheses in emit

--- src/Emit.hs
+++ src/Emit.hs
@@ -15,7 +15,7 @@ emit' (IntF i)       = show i
 emit' (CharF c)      = show c
 emit' (StrF s)       = show s
 emit' (VarF n)       = n
-emit' (AppF e1 e2)   = e1 ++ " " ++ e2
+emit' (AppF e1 e2)   = e1 ++ " " ++ paren e2
 emit' (LamF n e)     = paren ("fun " ++ n ++ " => " ++ e)
 emit' (TupleF es)    = paren $ intercalate ", " es
 emit' (LetF n e1 e2) = "let " ++ n ++ " = " ++ e1 ++ " in\n" ++ e2
