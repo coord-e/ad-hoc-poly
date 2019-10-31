2fad6cef4b1a91725a4d2af9ad6f73a6638daae7
coord.e <me@coord-e.com>
2019-10-31 06:45:00 +0000

    Fix: fun x -> x is correct in OCaml

--- src/Emit.hs
+++ src/Emit.hs
@@ -19,6 +19,6 @@ emit' (RealF f)      = show f
 emit' (BoolF b)      = bool "false" "true" b
 emit' (VarF n)       = n
 emit' (AppF e1 e2)   = e1 ++ " " ++ paren e2
-emit' (LamF n e)     = paren ("fun " ++ n ++ " => " ++ e)
+emit' (LamF n e)     = paren ("fun " ++ n ++ " -> " ++ e)
 emit' (TupleF es)    = paren $ intercalate ", " es
 emit' (LetF n e1 e2) = "let " ++ n ++ " = " ++ e1 ++ " in\n" ++ e2
