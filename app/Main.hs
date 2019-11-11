5661d3aadb4a5c09d321c6ea8528023f488d1552
coord.e <me@coord-e.com>
2019-11-11 09:43:11 +0000

    Change: Move compilation code to Compile module

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,15 @@
 module Main where
 
-import           Config             (Config, loadDefaultConfigFile)
-import           Emit               (emit)
-import           Overload           (compile)
-import           Parse              (parse)
+import           Compile            (compileFile)
 import           Reporting.Report   (printReport)
-import           Reporting.Result   (Result)
 
-import           Control.Monad      ((<=<))
-import           Data.Text          hiding (head)
-import qualified Data.Text.IO       as T
 import           System.Environment (getArgs)
 
-
-transpile :: Config -> Text -> Result String
-transpile c = fmap emit . compile c <=< parse
 
 main :: IO ()
 main = do
   args <- getArgs
-  content <- T.readFile $ head args
-  config <- loadDefaultConfigFile
-  case config >>= flip transpile content of
+  result <- compileFile Nothing $ head args
+  case result of
     Right output -> putStrLn output
     Left err     -> printReport err
