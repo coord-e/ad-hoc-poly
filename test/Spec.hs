2c128e2e542c8b45c772108013c0e63a705d7e7a
coord.e <me@coord-e.com>
2019-11-13 08:57:43 +0000

    Change: Delete redundant do

--- test/Spec.hs
+++ test/Spec.hs
@@ -43,25 +43,25 @@ shouldReturnContain a p = flip shouldContain p =<< a
 
 
 main :: IO ()
-main = hspec $ do
+main = hspec $
   describe "Overload resolution" $ do
-    it "handle constrainted instantiations" $ do
+    it "handle constrainted instantiations" $
       testSample "equality.mlx1" `shouldReturn` "false"
 
-    it "handle multi-parameter constraints" $ do
+    it "handle multi-parameter constraints" $
       testSample "into.mlx1" `shouldReturn` "42"
 
-    it "handle dictionaries" $ do
+    it "handle dictionaries" $
       testSample "number.mlx1" `shouldReturn` "13"
 
-    it "handle superclasses" $ do
+    it "handle superclasses" $
       testSample "superclass.mlx1" `shouldReturn` "true"
 
-    it "fails without necessary predicates" $ do
+    it "fails without necessary predicates" $
       testErrorSample "equality_err.mlx1" `shouldReturnContain` "Unable to instantiate"
 
-    it "fails with overlapping instances" $ do
+    it "fails with overlapping instances" $
       testErrorSample "overlap_err.mlx1" `shouldReturnContain` "overlapping"
 
-    it "fails with unmet superclass" $ do
+    it "fails with unmet superclass" $
       testErrorSample "superclass_err.mlx1" `shouldReturnContain` "Unresolved constraint"
