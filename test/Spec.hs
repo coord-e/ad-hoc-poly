12c1314cecb673be0e0f609bb0672af5bfc717ba
coord.e <me@coord-e.com>
2019-11-13 08:55:26 +0000

    Add: Add failing cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,11 +8,16 @@ import           Test.Hspec
 import           Compile          (compileFile)
 import           Reporting.Report (report)
 import           Reporting.Result (Result)
+import           Reporting.Error (Error)
 
 
 assertRight :: Result a -> a
 assertRight (Left err) = error $ report err
 assertRight (Right v)  = v
+
+assertLeft :: Show a => Result a -> Error
+assertLeft (Left err) = err
+assertLeft (Right v)  = error (show v)
 
 runML :: String -> IO Text
 runML source = withSystemTempFile ".ml" $ \file h -> do
@@ -22,10 +27,19 @@ runML source = withSystemTempFile ".ml" $ \file h -> do
   out <- shelly . print_stdout False $ run "ocaml" ["-w", "-26", pack file]
   return $ strip out
 
+compileFile' :: FilePath -> IO (Result String)
+compileFile' file = compileFile (Just "test/data/env.yaml") ("test/data/" ++ file)
+
 testSample :: FilePath -> IO Text
 testSample file = do
-  result <- assertRight <$> compileFile (Just "test/data/env.yaml") ("test/data/" ++ file)
+  result <- assertRight <$> compileFile' file
   runML result
+
+testErrorSample :: FilePath -> IO String
+testErrorSample file = report . assertLeft <$> compileFile' file
+
+shouldReturnContain :: (HasCallStack, Show a, Eq a) => IO [a] -> [a] -> Expectation
+shouldReturnContain a p = flip shouldContain p =<< a
 
 
 main :: IO ()
@@ -42,3 +56,12 @@ main = hspec $ do
 
     it "handle superclasses" $ do
       testSample "superclass.mlx1" `shouldReturn` "true"
+
+    it "fails without necessary predicates" $ do
+      testErrorSample "equality_err.mlx1" `shouldReturnContain` "Unable to instantiate"
+
+    it "fails with overlapping instances" $ do
+      testErrorSample "overlap_err.mlx1" `shouldReturnContain` "overlapping"
+
+    it "fails with unmet superclass" $ do
+      testErrorSample "superclass_err.mlx1" `shouldReturnContain` "Unresolved constraint"
